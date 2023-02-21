099a182a042570035e7cc067771d9f1357a90bec
SamTreon <stat123@hotmail.co.uk>
2023-02-21 12:29:41 +0000

    finished all the backend functionality, still need to add frontend styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,37 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
+  # Private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
